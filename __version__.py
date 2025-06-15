0fa0cca224e3f0e11f3d34246630ae5431677af4
ucheng0921 <pink_87921aa@outlook.com>
2025-06-15 19:23:11 +0800

    Release v1.2.7: 修正系統問題

--- __version__.py
+++ __version__.py
@@ -1,7 +1,7 @@
 """
 版本資訊
 """
-__version__ = "1.2.6"
-__build__ = "202506151848"
+__version__ = "1.2.7"
+__build__ = "202506151923"
 __author__ = "Artale Script Team"
 __description__ = "Artale 遊戲自動化腳本"
