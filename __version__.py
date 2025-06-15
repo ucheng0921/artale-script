4761e4e0f389f07ba9dbc6e8ffc847a608b20484
ucheng0921 <pink_87921aa@outlook.com>
2025-06-15 19:35:46 +0800

    Release v1.2.8: 復檢查更新功能題

--- __version__.py
+++ __version__.py
@@ -1,7 +1,7 @@
 """
 版本資訊
 """
-__version__ = "1.2.7"
-__build__ = "202506151923"
+__version__ = "1.2.8"
+__build__ = "202506151935"
 __author__ = "Artale Script Team"
 __description__ = "Artale 遊戲自動化腳本"
