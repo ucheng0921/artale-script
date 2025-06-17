db14dd72d8e587a85a83212c96ad01ed591421fd
ucheng0921 <pink_87921aa@outlook.com>
2025-06-17 22:37:58 +0800

    Release v1.2.12: 新增打包工具以及修復路徑問題

--- __version__.py
+++ __version__.py
@@ -1,7 +1,7 @@
 """
 版本資訊
 """
-__version__ = "1.2.11"
-__build__ = "202506161959"
+__version__ = "1.2.12"
+__build__ = "202506172237"
 __author__ = "Artale Script Team"
 __description__ = "Artale 遊戲自動化腳本"
