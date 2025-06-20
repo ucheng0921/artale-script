3f7d7d2a68201a1687e336273bd88b18ce347946
ucheng0921 <pink_87921aa@outlook.com>
2025-06-20 23:23:32 +0800

    Release v1.2.15: 修復原有功能v.2

--- __version__.py
+++ __version__.py
@@ -1,7 +1,7 @@
 """
 版本資訊
 """
-__version__ = "1.2.14"
-__build__ = "202506182223"
+__version__ = "1.2.15"
+__build__ = "202506202323"
 __author__ = "Artale Script Team"
 __description__ = "Artale 遊戲自動化腳本"
