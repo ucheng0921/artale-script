11fb51004469b5f19146ac4c73d29e30377827be
ucheng0921 <pink_87921aa@outlook.com>
2025-06-21 19:59:29 +0800

    Release v1.2.17: 怪物下載器v2.0

--- __version__.py
+++ __version__.py
@@ -1,7 +1,7 @@
 """
 版本資訊
 """
-__version__ = "1.2.16"
-__build__ = "202506211131"
+__version__ = "1.2.17"
+__build__ = "202506211959"
 __author__ = "Artale Script Team"
 __description__ = "Artale 遊戲自動化腳本"
